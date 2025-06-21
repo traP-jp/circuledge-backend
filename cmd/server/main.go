bbd9f73caa52c318fe38fc97a2fc1f17d312ae2c
ramdos0207 <dev@ramdos.net>
2025-06-21 11:40:11 +0900

    add middlewares

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"github.com/gorilla/sessions"
 	"github.com/traP-jp/circuledge-backend/cmd/server/server"
 	"github.com/traP-jp/circuledge-backend/pkg/config"
 	"github.com/traP-jp/circuledge-backend/pkg/database"
 
+	"github.com/labstack/echo-contrib/session"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -15,6 +17,14 @@ func main() {
 	// middlewares
 	e.Use(middleware.Recover())
 	e.Use(middleware.Logger())
+	e.Use(session.Middleware(sessions.NewCookieStore([]byte("secret"))))
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins:     []string{"https://circuledge.trap.show"},
+		AllowMethods:     []string{echo.GET, echo.POST, echo.PUT, echo.PATCH, echo.DELETE, echo.OPTIONS},
+		AllowHeaders:     []string{"*"},
+		ExposeHeaders:    []string{"*"},
+		AllowCredentials: true,
+	}))
 
 	// connect to and migrate database
 	db, err := database.Setup(config.MySQL())
